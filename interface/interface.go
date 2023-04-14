83f6380a78baf19527aada59e9130477c4421c8f
律威 <lvwei@sogou-inc.com>
2023-04-14 22:59:37 +0800

    多interface使用和使用指针接收者

--- interface/interface.go
+++ interface/interface.go
@@ -9,12 +9,16 @@ type Shape interface {
 	getArea() int
 }
 
+type Circumference interface {
+	getCircumference() int
+}
+
 type Rectangle struct {
 	width  int
 	height int
 }
 
-type circle struct {
+type Circle struct {
 	radius float64
 }
 
@@ -22,8 +26,16 @@ func (r Rectangle) getArea() int {
 	return r.width * r.height
 }
 
-func (c circle) getArea() int {
+func (r *Rectangle) getCircumference() int {
+	return 2 * (r.width + r.height)
+}
+
+func (c Circle) getArea() int {
 	return int(math.Pi * math.Pow(c.radius, 2))
+}
+
+func (c *Circle) getCircumference() int {
+	return int(2 * math.Pi * c.radius)
 }
 
 func outputAllArea(shapes []Shape) {
@@ -33,13 +45,22 @@ func outputAllArea(shapes []Shape) {
 	}
 }
 
+func outputAllCircumference(circumferences []Circumference) {
+	for i := 0; i < len(circumferences); i++ {
+		fmt.Println("Circumference:", circumferences[i].getCircumference())
+	}
+}
+
 func main() {
 
 	r1 := Rectangle{2, 5}
 	r2 := Rectangle{3, 4}
-	c1 := circle{3}
-	c2 := circle{4}
+	c1 := Circle{3}
+	c2 := Circle{4}
 
 	shapes := []Shape{r1, r2, c1, c2}
 	outputAllArea(shapes)
+
+	circumferences := []Circumference{&r1, &r2, &c1, &c2}
+	outputAllCircumference(circumferences)
 }
