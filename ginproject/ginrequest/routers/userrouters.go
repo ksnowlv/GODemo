6488d5934ae85d53f93590f28032f2ae3e5aafa5
律威 <lvwei@sogou-inc.com>
2023-05-01 21:39:54 +0800

    user相关接口开启gzip压缩

--- ginproject/ginrequest/routers/userrouters.go
+++ ginproject/ginrequest/routers/userrouters.go
@@ -23,7 +23,7 @@ func UserRoutersInit(r *gin.Engine) {
 		 -H 'Content-Type: application/json' \
 		 -H 'Accept-Encoding: gzip, deflate, br' \
 		 -H 'Connection: keep-alive' \
-		 -H 'User-Agent: ksnowlv关于Gin框架学习实践' \
+		 -H 'User-Agent: ksnowlv关于Gin框架学习实践'
 		*/
 		userGroup.GET("/cookie", user.XUserController{}.Cookie)
 
@@ -50,8 +50,11 @@ func UserRoutersInit(r *gin.Engine) {
 		//POST form表单提交 curl中使用--data,而不是-data；--data如果有多个键值对，必须加上双引号
 		/*
 			curl --data "phone=152&code=152" "http://localhost:8080/user/login_form"
+
+			curl --data "phone=152&code=152" "http://localhost:8080/user/login_form" > tmp.gz ; gzip -d tmp.gz & cat tmp
 		*/
 
+		userGroup.POST("/login_form", user.XUserController{}.UserLoginWithForm)
 		/* -d/data HTTP POST方式传送数据   -D/–dump-header 把header信息写入到该文件中
 				 curl -X POST http://localhost:8080/user/add \
 		 		  -d '{"name": "ksnowlv", "age": 30,"Phone": "152"}' \
@@ -75,8 +78,24 @@ func UserRoutersInit(r *gin.Engine) {
 				  -H 'Content-Type: application/json' \
 				  -H 'Accept-Encoding: gzip, deflate, br' \
 				  -H 'Connection: keep-alive' \
-				  -H 'User-Agent: ksnowlv关于Gin框架学习实践'
+				  -H 'User-Agent: ksnowlv关于Gin框架学习实践' \
+				  -H 'Accept: application/json, text/html'  > tmp.gz ; gzip -d tmp.gz & cat tmp
 		*/
 		userGroup.POST("/getalluser", user.XUserController{}.GetAllUser)
+
+		/* -d/data HTTP POST方式传送数据   -D/–dump-header 把header信息写入到该文件中
+		curl -X GET http://localhost:8080/user/getallusers \
+		 -H 'Content-Type: application/json' \
+		 -H 'Accept-Encoding: gzip, deflate, br' \
+		 -H 'Connection: keep-alive' \
+		 -H 'User-Agent: ksnowlv关于Gin框架学习实践' > tmp.gz ; gzip -d tmp.gz & cat tmp
+
+		 curl -X GET http://localhost:8080/user/getallusers \
+		 -H 'Content-Type: application/json' \
+		 -H 'Accept-Encoding: gzip, deflate, br' \
+		 -H 'Connection: keep-alive' \
+		 -H 'User-Agent: ksnowlv关于Gin框架学习实践' --output tmp.gz;cat tmp
+		*/
+		userGroup.GET("/getallusers", user.XUserController{}.GetAllUser)
 	}
 }
